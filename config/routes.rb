98d4e6eead134ef71f6f4a40bf41ede09ba429a0
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-25 23:48:53 -0300

    add new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   #define o index de articles como root
   root "articles#index"
-  get "/articles", to:"articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/articles", to:"articles#index"
+  get "/articles/:id", to: "articles#show"
+
 end
