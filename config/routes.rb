c2cf1144ecc25c5060ece7b3d60c3ab7b43f6eff
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-25 23:54:40 -0300

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   #define o index de articles como root
   root "articles#index"
 
-  get "/articles", to:"articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
 end
