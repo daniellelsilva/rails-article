04426ad0075617ac01f45d23a8b91cd5c3ae7e05
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-26 22:23:22 -0300

    create association between models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  #associação com o model de comments
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
