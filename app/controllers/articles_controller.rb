732e371bbd9d9da1eefcb62d3059193fc773f6d1
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-26 21:58:57 -0300

    add edit actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  #altera mas não salva
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  #salva a edição
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
