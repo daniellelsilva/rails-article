60660e6f1e4f24134322596ca21f8338e698507d
Danielle Silva <danielle.luisasilva@gmail.com>
2022-06-29 22:54:04 -0300

    iniate tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/views/**/*'
   ],
   theme: {
     extend: {
@@ -13,6 +14,12 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    fontSize: {
+      'sm': '1rem',
+      'md': '1.5rem',
+      'lg': '2rem',
+      'xlg': '3rem',
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
